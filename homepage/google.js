679c50d81ed09591d035a80f79560b42cdd942d8
thomascds-creator <kelenikelenichannel@gmail.com>
2021-05-17 14:07:55 +0300

    homepagesection for index file

---
+++ homepage/google.js
@@ -0,0 +1,29 @@
+
+let inputbox = document.getElementById('inputbox')
+
+
+
+inputbox.onmouseover = () =>{
+    
+    
+    inputbox.style.boxShadow = '0px 0px 5px 2px #DFE1E5';
+    
+
+}
+
+inputbox.onmouseout = () =>{
+    
+     inputbox.style.boxShadow = '0px 0px';
+    
+     
+    
+}
+
+
+
+
+
+
+
+
+
